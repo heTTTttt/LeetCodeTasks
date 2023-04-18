c855c362329a4de8430afad8cbed963910b486e5
heTTTttt <rock.n.roll.one.love@gmail.com>
2023-04-18 17:46:03 +0300

    Main class using to test Task methods

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import solutions.Tasks;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Tasks tasks = new Tasks();
+
+        System.out.println(tasks.isPalindromeNumber(-21212));
+
+    }
+}
